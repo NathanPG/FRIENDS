5db8c8ba74d3567249bb1ba43856328263cc0bd8
Helen <wur7@rpi.edu>
2019-09-29 21:59:50 -0400

    ui

---
+++ Assets/Login_Interface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Login_Interface : MonoBehaviour
+{
+    public Text username;
+    public GameObject warningPanel;
+    public void Login_UI()
+    {
+        if(username.text!="")
+        {
+            SceneManager.LoadScene(4);
+        }
+        else
+        {
+            warningPanel.SetActive(true);
+        }
+    }
+
+    public void WarmingDisapper()
+    {
+        warningPanel.SetActive(false);
+    }
+}
