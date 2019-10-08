c5c29a6ee3c2475fedec231a8498efabb48f7c11
NathanPG <nqpenguin@gmail.com>
2019-10-08 12:43:57 -0400

    updated UI and net framework

---
+++ Assets/Scripts/LoginUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class LoginUI : NetworkBehaviour
+{
+    public string Port;
+    public string IpAddress;
+    public Text username;
+    bool isStart;
+    //public GameObject warningPanel;
+
+    //TODO: MORE PORT NUMBERS
+    public void LoginOnClick()
+    {
+        NetworkManager.singleton.networkPort = 9878;
+        NetworkManager.singleton.StartClient();
+        SceneManager.LoadScene(2);
+        /*
+        if (username.text != "")
+        {
+            //
+        }
+        else
+        {
+            //warningPanel.SetActive(true);
+        }
+        */
+    }
+
+    //THIS IS GOING TO BE A SERVER
+    //WE ARE USING IT AS A HOST NOW
+    public void AdminOnClick()
+    {
+        NetworkManager.singleton.networkAddress = "localhost";
+        NetworkManager.singleton.networkPort = 9877;
+        NetworkManager.singleton.StartHost();
+        SceneManager.LoadScene(2);
+    }
+
+    private void Start()
+    {
+        isStart = false;
+    }
+}
---
+++ Assets/Scripts/OLScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class OLScene : NetworkBehaviour
+{
+    public void DisconnectOnClick()
+    {
+        if (isClient)
+        {
+            NetworkManager.singleton.StopClient();
+        }
+        else
+        {
+            NetworkManager.singleton.StopHost();
+        }
+        SceneManager.LoadScene(0);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
