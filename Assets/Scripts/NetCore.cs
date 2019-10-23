5be60a6280367c51885565ff7714d892fa89a268
NathanPG <nqpenguin@gmail.com>
2019-10-23 14:41:18 -0400

    panel set

--- Assets/Scripts/NetCore.cs
+++ Assets/Scripts/NetCore.cs
@@ -10,163 +10,6 @@ public class NetCore : MonoBehaviour
     GameObject spawned_player;
     NetworkClient myClient;
     public bool isPlayer;
-
-    void OnClinetConnected(NetworkMessage netMsg)
-    {
-        /*
-        // Do stuff when connected to the server
-
-        MyNetworkMessage messageContainer = new MyNetworkMessage();
-        messageContainer.message = "Hello server!";
-
-        // Say hi to the server when connected
-        myClient.Send(1000, messageContainer);
-        */
-        Debug.Log("CLIENT CONNECTED!");
-    }
-
-    // Register the handlers for the different message types
-    void RegisterClientHandlers()
-    {
-        // Unity have different Messages types defined in MsgType
-        //client.RegisterHandler(messageID, OnMessageReceived);
-        myClient.RegisterHandler(MsgType.Connect, OnClinetConnected);
-        //client.RegisterHandler(MsgType.Disconnect, OnDisconnected);
-    }
-
-    // Create a client and connect to the server port
-    public void SetupClient()
-    {
-        int port = 9999;
-        string ip = "localhost";
-
-        // The id we use to identify our messages and register the handler
-        short TESTID = 1000;
-
-        var config = new ConnectionConfig();
-
-        // Config the Channels we will use
-        config.AddChannel(QosType.ReliableFragmented);
-        config.AddChannel(QosType.UnreliableFragmented);
-
-        // Create the client ant attach the configuration
-        myClient = new NetworkClient();
-        myClient.Configure(config, 1);
-
-        // Register the handlers for the different network messages
-        RegisterClientHandlers();
-
-        // Connect to the server
-        myClient.Connect(ip, port);
-
-        var TESTMSG = new StringMessage("CLIENT MIAO IS HERE!");
-        //myClient.Send(TESTID, TESTMSG);
-        /*
-        void OnDisconnected(NetworkMessage message)
-        {
-            // Do stuff when disconnected to the server
-        }
-
-        
-        // Message received from the server
-        void OnMessageReceived(NetworkMessage netMessage)
-        {
-            // You can send any object that inherence from MessageBase
-            // The client and server can be on different projects, as long as the MyNetworkMessage or the class you are using have the same implementation on both projects
-            // The first thing we do is deserialize the message to our custom type
-            var objectMessage = netMessage.ReadMessage<MyNetworkMessage>();
-
-            Debug.Log("Message received: " + objectMessage.message);
-        }
-        */
-    }
-
-    
-
-    void OnApplicationQuit()
-    {
-        NetworkServer.Shutdown();
-
-    }
-
-    public void SetupServer()
-    {
-        int port = 9999;
-        int maxConnections = 10;
-
-        // The id we use to identify our messages and register the handler
-        short messageID = 1000;
-
-        Application.runInBackground = true;
-        // Use this for initialization
-        // Register handlers for the types of messages we can receive
-        //RegisterServerHandlers();
-
-        var config = new ConnectionConfig();
-        // There are different types of channels you can use, check the official documentation
-        config.AddChannel(QosType.ReliableFragmented);
-        config.AddChannel(QosType.UnreliableFragmented);
-
-        var ht = new HostTopology(config, maxConnections);
-        if (!NetworkServer.Configure(ht))
-        {
-            Debug.Log("No server created, error on the configuration definition");
-            return;
-        }
-        else
-        {
-            // Start listening on the defined port
-            if (NetworkServer.Listen(port))
-                Debug.Log("Server created, listening on port: " + port);
-            else
-                Debug.Log("No server created, could not listen to the port: " + port);
-        }
-        
-        /*
-        void OnClientConnected(NetworkMessage netMessage)
-        {
-            // Do stuff when a client connects to this server
-
-            // Send a thank you message to the client that just connected
-            MyNetworkMessage messageContainer = new MyNetworkMessage();
-            messageContainer.message = "Thanks for joining!";
-
-            // This sends a message to a specific client, using the connectionId
-            NetworkServer.SendToClient(netMessage.conn.connectionId, messageID, messageContainer);
-
-            // Send a message to all the clients connected
-            messageContainer = new MyNetworkMessage();
-            messageContainer.message = "A new player has conencted to the server";
-
-            // Broadcast a message a to everyone connected
-            NetworkServer.SendToAll(messageID, messageContainer);
-        }
-
-        void OnClientDisconnected(NetworkMessage netMessage)
-        {
-            // Do stuff when a client dissconnects
-        }
-
-        void OnMessageReceived(NetworkMessage netMessage)
-        {
-            // You can send any object that inherence from MessageBase
-            // The client and server can be on different projects, as long as the MyNetworkMessage or the class you are using have the same implementation on both projects
-            // The first thing we do is deserialize the message to our custom type
-            var objectMessage = netMessage.ReadMessage<MyNetworkMessage>();
-            Debug.Log("Message received: " + objectMessage.message);
-
-        }
-        */
-
-    }
-
-    /*
-    void OnApplicationQuit()
-    {
-        NetworkServer.Shutdown();
-    }
-    */
-
     const short MESSAGE_ID = 1002;
     const short MESSAGE_ID0 = 1003;
 
@@ -175,16 +18,18 @@ public class NetCore : MonoBehaviour
         Debug.Log("CLINET CONNECTED!");
         NetworkManager.singleton.client.Send(MESSAGE_ID, new StringMessage("MIAO"));
     }
-
+    
     public void OnServerReceive(NetworkMessage netMsg)
     {
         Debug.Log("Server Received MSG!" + netMsg);
         NetworkServer.SendToAll(MESSAGE_ID0, new StringMessage("I HEARED YOUR MIAO"));
     }
+
     public void OnClientReceive(NetworkMessage netMsg)
     {
         Debug.Log("Client Received MSG!" + netMsg);
     }
+
     bool msg_sent = false;
     private void Update()
     {
@@ -200,6 +45,7 @@ public class NetCore : MonoBehaviour
             NetworkManager.singleton.client.Send(MESSAGE_ID, new StringMessage("MIAO"));
         }
     }
+
     private void Start()
     {
         
@@ -234,7 +80,11 @@ public class NetCore : MonoBehaviour
             //spawned_player = Instantiate(player_pre);
             //spawned_player.GetComponent<Player>().isPlayer = false;
             //NetworkServer.Spawn(spawned_player);
-            
         }
+    }
+
+    void OnApplicationQuit()
+    {
+        NetworkServer.Shutdown();
     }
 }
