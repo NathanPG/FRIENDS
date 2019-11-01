076ffac23601985b13b590fce1c826bf7f4d2350
jasonht233 <jasonzht233@gmail.com>
2019-10-31 22:05:50 -0400

    add Connection between SQLHandler and OutputFileSys
    add the LOAD File (untest

--- Assets/Scripts/ProfileSys.cs
+++ Assets/Scripts/ProfileSys.cs
@@ -17,4 +17,27 @@ public class ProfileSys : MonoBehaviour
 
     }
     */
+
+    public void UpdateResult(Dictionary<string, Dictionary<string, string>> sourceDict)
+    {
+        foreach (KeyValuePair<string, Dictionary<string, string>> itr in sourceDict)
+        {
+            string qid = itr.Value["id"].ToString();
+            string title = itr.Value["title"].ToString();
+            string content = itr.Value["content"].ToString();
+            string coin = itr.Value["coin"].ToString();
+            string owner = itr.Value["owner"].ToString();
+            string exp = itr.Value["exp"].ToString();
+
+            this.Task_List[qid] = new Dictionary<string, string>();
+            this.Task_List[qid]["QID"] = qid;
+            this.Task_List[qid]["content"] = content;
+            this.Task_List[qid]["title"] = title;
+            this.Task_List[qid]["exp"] = exp;
+            this.Task_List[qid]["coin"] = coin;
+            this.Task_List[qid]["onwer"] = owner;
+
+        }
+    }
+
 }
